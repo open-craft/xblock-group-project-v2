00b1c967179507ba2ab24c4c39cb963c2c299883
E. Kolpakov <eugeny.kolpakov@gmail.com>
2015-05-28 17:59:02 +0300

    Removed sections and fixed XML parser tests. Ordinary rendering is broken in this commit

--- tests/unit/test_xml_parsing.py
+++ tests/unit/test_xml_parsing.py
@@ -2,14 +2,15 @@
 # from django import settings
 
 import sys
+import textwrap
 from unittest import TestCase
 from datetime import date
 from group_project_v2.group_activity import GroupActivity, STAGE_TYPES
 
 
 class GroupActivitityXmlTest(TestCase):
-    def _assert_stage_name_and_type(self, stage, expected_name, expected_type):
-        self.assertEqual(stage.name, expected_name)
+    def _assert_stage_name_and_type(self, stage, expected_title, expected_type):
+        self.assertEqual(stage.title, expected_title)
         self.assertEqual(stage.type, expected_type)
 
     def _assert_stage_start_and_end_date(self, stage, expected_open, expected_close):
@@ -33,75 +34,72 @@ class GroupActivitityXmlTest(TestCase):
     def test_read_from_xml(self):
         grp_act = GroupActivity.import_xml_file('tests/xml/test.xml')
 
-        ir = list(grp_act.resources)
-        self.assertEqual(len(ir), 2)
-        self.assertEqual(ir[0]["title"], "Issue Tree Template")
-        self.assertEqual(ir[0]["description"], None)
-        self.assertEqual(ir[0]["location"], "http://download/file.doc")
-        self.assertEqual(ir[1]["description"], "These are the instructions for this activity")
+        resource_data = list(grp_act.resources)
+        self.assertEqual(len(resource_data), 2)
+        self.assertEqual(resource_data[0]["title"], "Issue Tree Template")
+        self.assertEqual(resource_data[0]["description"], None)
+        self.assertEqual(resource_data[0]["location"], "http://download/file.doc")
+        self.assertEqual(resource_data[1]["description"], "These are the instructions for this activity")
 
-        gc = list(grp_act.grading_criteria)
-        self.assertEqual(len(gc), 1)
+        grading_criteria_data = list(grp_act.grading_criteria)
+        self.assertEqual(len(grading_criteria_data), 1)
 
-        sr = list(grp_act.submissions)
-        self.assertEqual(len(sr), 3)
-        self.assertEqual(sr[0]["id"], "issue_tree")
-        self.assertEqual(sr[0]["title"], "Issue Tree")
-        self.assertEqual(sr[0]["description"], None)
-        self.assertEqual(sr[2]["description"], "xls budget plan")
+        submissions_data = list(grp_act.submissions)
+        self.assertEqual(len(submissions_data), 3)
+        self.assertEqual(submissions_data[0]["id"], "issue_tree")
+        self.assertEqual(submissions_data[0]["title"], "Issue Tree")
+        self.assertEqual(submissions_data[0]["description"], None)
+        self.assertEqual(submissions_data[2]["description"], "xls budget plan")
 
-        ac = grp_act.activity_stages
-        self.assertEqual(len(ac), 6)
-        self._assert_stage_name_and_type(ac[0], "Overview", STAGE_TYPES.NORMAL)
-        self._assert_stage_name_and_type(ac[1], "Upload", STAGE_TYPES.NORMAL)
-        self._assert_stage_name_and_type(ac[2], "Review Team", STAGE_TYPES.PEER_REVIEW)
-        self._assert_stage_name_and_type(ac[3], "Review Group", STAGE_TYPES.GROUP_REVIEW)
-        self._assert_stage_name_and_type(ac[4], "Evaluate Team Feedback", STAGE_TYPES.PEER_ASSESSMENT)
-        self._assert_stage_name_and_type(ac[5], "Evaluate Group Feedback", STAGE_TYPES.GROUP_ASSESSMENT)
+        stages = grp_act.activity_stages
+        self.assertEqual(len(stages), 6)
 
-        self._assert_stage_start_and_end_date(ac[0], None, None)
-        self._assert_stage_start_and_end_date(ac[1], None, date(2014, 5, 24))
-        self._assert_stage_start_and_end_date(ac[2], date(2014, 5, 24), date(2014, 6, 20))
-        self._assert_stage_start_and_end_date(ac[3], date(2014, 5, 24), date(2014, 6, 20))
-        self._assert_stage_start_and_end_date(ac[4], date(2014, 6, 20), None)
-        self._assert_stage_start_and_end_date(ac[5], date(2014, 6, 20), None)
+        overview, upload, team_review, group_review, team_assessment, group_assessment = stages
 
-        self._assert_resources_submissions_and_grading(ac[0], resources=2, submissions=0, grading_criteria=0)
-        self._assert_resources_submissions_and_grading(ac[1], resources=0, submissions=3, grading_criteria=1)
-        self._assert_resources_submissions_and_grading(ac[2], resources=0, submissions=0, grading_criteria=0)
-        self._assert_resources_submissions_and_grading(ac[3], resources=0, submissions=0, grading_criteria=0)
+        self._assert_stage_name_and_type(overview, "Overview", STAGE_TYPES.NORMAL)
+        self._assert_stage_name_and_type(upload, "Upload", STAGE_TYPES.UPLOAD)
+        self._assert_stage_name_and_type(team_review, "Review Team", STAGE_TYPES.PEER_REVIEW)
+        self._assert_stage_name_and_type(group_review, "Review Group", STAGE_TYPES.GROUP_REVIEW)
+        self._assert_stage_name_and_type(team_assessment, "Evaluate Team Feedback", STAGE_TYPES.PEER_ASSESSMENT)
+        self._assert_stage_name_and_type(group_assessment, "Evaluate Group Feedback", STAGE_TYPES.GROUP_ASSESSMENT)
 
-        self.assertEqual(len(ac[0].sections), 4)
-        self.assertEqual(ac[0].sections[0].title, "Section Title")
-        self.assertEqual(ac[0].sections[1].title, "Details")
-        self.assertEqual(ac[0].sections[2].title, "Suggested Schedule")
-        self.assertEqual(ac[0].sections[3].title, "Project Materials")
+        self._assert_stage_start_and_end_date(overview, None, None)
+        self._assert_stage_start_and_end_date(upload, None, date(2014, 5, 24))
+        self._assert_stage_start_and_end_date(team_review, date(2014, 5, 24), date(2014, 6, 20))
+        self._assert_stage_start_and_end_date(group_review, date(2014, 5, 24), date(2014, 6, 20))
+        self._assert_stage_start_and_end_date(team_assessment, date(2014, 6, 20), None)
+        self._assert_stage_start_and_end_date(group_assessment, date(2014, 6, 20), None)
 
-        pm = ac[0].sections[3]
-        self.assertEqual(list(pm.file_links), ir)
-        self.assertEqual(pm.file_link_name, "resources")
+        self._assert_resources_submissions_and_grading(overview, resources=2, submissions=0, grading_criteria=0)
+        self._assert_resources_submissions_and_grading(upload, resources=0, submissions=3, grading_criteria=1)
+        self._assert_resources_submissions_and_grading(team_review, resources=0, submissions=0, grading_criteria=0)
+        self._assert_resources_submissions_and_grading(group_review, resources=0, submissions=0, grading_criteria=0)
 
-        self.assertNotEqual(ac[0].sections[0].content, None)
-        self.assertEqual(ac[0].sections[0].content_html, "<p>Html Description Blah Blah Blah<span>Additional info</span></p>")
+        self.assertEqual(list(overview.resources), resource_data)
 
-        sl = ac[1].sections[1]
-        self.assertEqual(list(sl.file_links), sr)
+        self.assertNotEqual(overview.content, None)
+        self.assertEqual(textwrap.dedent(overview.content_html), textwrap.dedent(
+            """
+            <p>Html Description Blah Blah Blah<span>Additional info</span></p>
+            <p>Html Description Blah Blah Blah</p>
+            <p>Html Description Blah Blah Blah</p>
+            <p>Html Description Blah Blah Blah</p>
+            """))
 
-        self._assert_stage_sections(ac[2], normal=1, peer_review=2)
-        self._assert_stage_sections(ac[3], group_review=2)
-        self._assert_stage_sections(ac[4], normal=1, peer_assessment=1)
-        self._assert_stage_sections(ac[5], group_assessment=1)
+        self.assertEqual(list(upload.submissions), submissions_data)
 
-        self.assertEqual(len(ac[2].sections), 1)
-        self.assertEqual(len(ac[2].peer_review_sections), 2)
-        self.assertEqual(len(ac[2].other_group_sections), 0)
-        self.assertEqual(len(ac[3].other_group_sections), 2)
-        self.assertEqual(len(ac[2].peer_review_sections[0].questions), 1)
-        self.assertEqual(len(ac[2].peer_review_sections[1].questions), 2)
+        self.assertEqual(team_review.questions[0].answer_html, '<input placeholder="answer here" type="text" />')
 
-        self.assertEqual(ac[2].peer_review_sections[0].questions[0].answer_html, '<input placeholder="answer here" type="text" />')
+        self.assertEqual(team_review.questions[0].id, "score")
+        self.assertEqual(team_review.questions[1].id, "q1")
+        self.assertEqual(team_review.questions[2].id, "q2")
+        self.assertEqual(team_review.questions[0].required, True)
+        self.assertEqual(team_review.questions[1].required, True)
+        self.assertEqual(team_review.questions[2].required, False)
 
-        self.assertEqual(ac[2].peer_review_sections[1].questions[0].id, "q1")
-        self.assertEqual(ac[2].peer_review_sections[1].questions[1].id, "q2")
-        self.assertEqual(ac[2].peer_review_sections[1].questions[0].required, True)
-        self.assertEqual(ac[2].peer_review_sections[1].questions[1].required, False)
+        self.assertEqual(group_review.questions[0].id, "score")
+        self.assertEqual(group_review.questions[1].id, "q1")
+        self.assertEqual(group_review.questions[2].id, "q2")
+        self.assertEqual(group_review.questions[0].required, True)
+        self.assertEqual(group_review.questions[1].required, True)
+        self.assertEqual(group_review.questions[2].required, True)
