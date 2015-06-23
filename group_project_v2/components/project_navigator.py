0e1b6ee579721a558bc1752e5e49a0d42798b44d
E. Kolpakov <eugeny.kolpakov@gmail.com>
2015-06-23 16:41:47 +0300

    Rendering navigator children and seletor buttons

--- group_project_v2/components/project_navigator.py
+++ group_project_v2/components/project_navigator.py
@@ -1,24 +1,53 @@
-import logging
-
 from xblock.core import XBlock
 from xblock.fields import Scope, String, Dict, Float, Integer
 from xblock.fragment import Fragment
 
-from xblockutils.resources import ResourceLoader
 from xblockutils.studio_editable import StudioContainerXBlockMixin
 
-log = logging.getLogger(__name__)
-loader = ResourceLoader(__name__)
+from ..utils import loader
 
 
 class GroupProjectNavigatorXBlock(StudioContainerXBlockMixin, XBlock):
+    display_name_with_default = "Group Project Navigator"
+
+    editable = False
     has_score = False
     has_children = True
 
+    @property
+    def group_project(self):
+        return self.get_parent()
+
     def student_view(self, context):
         fragment = Fragment()
-        self.render_children(context, fragment, can_reorder=False, can_add=False)
+        children_items = []
+        for child_id in self.children:
+            child = self.runtime.get_block(child_id)
+            # child_fragment = child.render('student_view', context)
+            child_fragment = child.student_view(context)
+
+            fragment.add_frag_resources(child_fragment)
+            children_items.append({
+                'type': child.type,
+                'display_name': child.display_name_with_default,
+                'content': child_fragment.content
+            })
+
+        fragment.add_content(
+            loader.render_template('templates/html/group_project_navigator.html', {'children': children_items})
+        )
+        fragment.add_css_url(
+            self.runtime.local_resource_url(self.group_project, 'public/css/group_project_navigator.css')
+        )
+
         return fragment
+
+    def author_preview_view(self, context):
+        """
+        Child blocks can override this to add a custom preview shown to authors in Studio when
+        not editing this block's children.
+        """
+        return self.student_view(context)
 
     def author_edit_view(self, context):
         """
@@ -27,33 +56,45 @@ class GroupProjectNavigatorXBlock(StudioContainerXBlockMixin, XBlock):
         fragment = Fragment()
         self.render_children(context, fragment, can_reorder=True, can_add=False)
         fragment.add_content(loader.render_template('templates/html/group_project_navigator_add_buttons.html', {}))
-        fragment.add_css_url(self.runtime.local_resource_url(self, 'public/css/group_project_edit.css'))
+        fragment.add_css_url(self.runtime.local_resource_url(self.group_project, 'public/css/group_project_edit.css'))
         return fragment
 
 
 class NavigationViewXBlock(XBlock):
-    def student_view(self):
+    display_name_with_default = "Navigation View"
+    type = "navigation"
+
+    def student_view(self, context):
         fragment = Fragment()
-        fragment.add_content("<div>I'm navigator</div>")
+        fragment.add_content(u"I'm navigator")
         return fragment
 
 
 class ResourcesViewXBlock(XBlock):
-    def student_view(self):
+    display_name_with_default = "Resources View"
+    type = "resources"
+
+    def student_view(self, context):
         fragment = Fragment()
-        fragment.add_content("<div>I'm resources</div>")
+        fragment.add_content(u"I'm resources")
         return fragment
 
 
 class SubmissionsViewXBlock(XBlock):
-    def student_view(self):
+    display_name_with_default = "Submissions View"
+    type = "submissions"
+
+    def student_view(self, context):
         fragment = Fragment()
-        fragment.add_content("<div>I'm submissions</div>")
+        fragment.add_content(u"I'm submissions")
         return fragment
 
 
 class AskTAViewXBlock(XBlock):
-    def student_view(self):
+    display_name_with_default = "Ask a TA View"
+    type = "ask-ta"
+
+    def student_view(self, context):
         fragment = Fragment()
-        fragment.add_content("<div>I'm ask a TA</div>")
+        fragment.add_content(u"I'm ask a TA")
         return fragment
