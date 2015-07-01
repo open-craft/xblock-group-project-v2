04205b08593ea6cc3b4bfd1bc84b2a7719849bdb
E. Kolpakov <eugeny.kolpakov@gmail.com>
2015-07-01 11:26:52 +0300

    Added mock project_api implementation

--- tests/integration/base_test.py
+++ tests/integration/base_test.py
@@ -1,8 +1,16 @@
+from mock import Mock
+
 from xblockutils.base_test import SeleniumXBlockTest
+
+import group_project_v2.project_api as project_api_module
 from tests.utils import loader
 
 
 class BaseIntegrationTest(SeleniumXBlockTest):
+    def setUp(self):
+        # monkeypatching project_api
+        project_api_module.project_api = Mock(spec=project_api_module.ProjectAPI)
+
     def load_scenario(self, xml_file, params=None, load_immediately=True):
         """
         Given the name of an XML file in the xml_templates folder, load it into the workbench.
