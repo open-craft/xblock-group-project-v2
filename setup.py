2a2418febc2de3ffa58bc4ff0e30e4d34fa565fc
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2019-02-08 02:38:57 -0200

    Hotfix setup issue

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         'pytz',
         'XBlock>=1.2.2,<2.0',
         'xblock-utils>=0.9',
+        'django-upload-validator==1.0.2',
         'edx-opaque-keys>=0.4'
     ],
     entry_points={
@@ -42,7 +43,7 @@ setup(
     },
     dependency_links = [
         'https://github.com/edx/xblock-utils/tarball/v1.0.5#egg=xblock-utils-1.0.5',
-        'https://github.com/mckinseyacademy/django-upload-validator/tarball/v1.0.2#egg=django-upload-validator==v1.0.2'
+        'https://github.com/mckinseyacademy/django-upload-validator/tarball/v1.0.2#egg=django-upload-validator-1.0.2'
     ],
     package_data=package_data("group_project_v2", ["templates", "public"]),
 )
