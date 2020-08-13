f9fa45ffbaf36942ea6ff92c9d6bc07c2fb4d072
Nasir Hussain <nasirhjafri@gmail.com>
2020-08-13 13:55:50 +0100

    Update setup.py
    Co-authored-by: Kshitij Sobti <kshitij@sobti.in>

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def package_data(pkg, root_list):
 
 setup(
     name='xblock-group-project-v2',
-    version='0.7.3',
+    version='0.8.0',
     description='XBlock - Group Project V2',
     packages=find_packages(),
     install_requires=[
